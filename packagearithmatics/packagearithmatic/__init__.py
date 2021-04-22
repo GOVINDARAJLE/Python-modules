21de27f2e9532b5cd0f8401c20e6b3225b7134b5
GOVINDA RAJLE <govindarajle1213@gmail.com>
2021-04-22 17:39:14 +0530

    added files of package arithmatic module

---
+++ packagearithmatics/packagearithmatic/__init__.py
@@ -0,0 +1,3 @@
+def add(a,b):
+    Result=a+b
+    return Result       
---
+++ packagearithmatics/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+setup(name="packagearithmatic",
+version="0.1",
+description="Arithmatic Operators functions.",
+Long_description="This package contains all Arithmatic operators function to use when ever need.",
+author="MG-Mate",
+packages=['packagearithmatic'],
+install_requires=[]
+
+)
